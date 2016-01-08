b7bc60aab963280dccfcfe0531b8e03dc0db4128
Sebastian Walter <s.walter@global-concepts.de>
2016-01-08 09:15:22 +0100

    added domain tab

---
+++ WinSPCheck/Extension/ControlHelpers.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace WinSPCheck.Extension
+{
+    /// <summary>
+    ///     Extentionclass for Controls
+    /// </summary>
+    public static class ControlHelpers
+    {
+        /// <summary>
+        ///     returns Visibility by bool.
+        /// </summary>
+        /// <param name="isVisible"></param>
+        /// <returns></returns>
+        public static Visibility ToVisibility(this bool isVisible)
+        {
+            return isVisible ? Visibility.Visible : Visibility.Hidden;
+        }
+    }
+}
